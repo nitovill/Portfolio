8cd4f6cd3de0a1799e5da7c34f452dbadab9c81d
nitovillafuerte <nitovillafuerte@outlook.com>
2021-07-31 21:38:07 -0300

    Portfolio en camino

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,20 +2,58 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 import "./Profile.css";
 import myImage from "../../assets/images/download.png";
-import Timeline from "../Timeline/Timeline";
+import Timeline, { CustomTimelineSeparator } from "../Timeline/Timeline";
+import resumeData from "../../utils/resumeData";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+
+const CustomTimeLineItem = ({ title, text, link }) => (
+  <TimelineItem>
+    <CustomTimelineSeparator />
+    <TimelineContent className="timeline_content">
+      {link ? (
+        <Typography className="timelineItem_text">
+          <span>{title}:</span>
+          {""}
+          <a href={link} target="_blank">
+            {text}
+          </a>
+        </Typography>
+      ) : (
+        <Typography className="timelineItem_text">
+          <span>{title}:</span>
+          {text}
+        </Typography>
+      )}
+    </TimelineContent>
+  </TimelineItem>
+);
 
 const Profile = () => {
   return (
     <div className="profile container_shadow">
       <div className="profile_name">
-        <Typography className="name">Villafuerte Nelson Mariano</Typography>
-        <Typography className="title">Full Stack Developer</Typography>
+        <Typography className="name">{resumeData.name}</Typography>
+        <Typography className="title">{resumeData.title}</Typography>
       </div>
       <figure className="profile_image">
         <img src={myImage} alt="hola" />
       </figure>
       <div className="profile_information">
-        <Timeline />
+        <Timeline icon={<PersonOutlineIcon />}>
+          <CustomTimeLineItem title="name" text={resumeData.name} />
+          <CustomTimeLineItem title="title" text={resumeData.title} />
+          <CustomTimeLineItem title="email" text={resumeData.email} />
+          <CustomTimeLineItem title="phone" text={resumeData.phone} />
+          {Object.keys(resumeData.social).map((key) => (
+            <CustomTimeLineItem
+              title={key}
+              text={resumeData.social[key].text}
+              link={resumeData.social[key].link}
+            />
+          ))}
+        </Timeline>
         <br />
         <button>My button</button>
       </div>
---
+++ src/utils/resumeData.js
@@ -0,0 +1,22 @@
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+export default {
+  name: "Mariano Villafuerte",
+  title: "Full stack developer",
+  cumpleaños: "30 de Octubre, 1995",
+  email: "nitovillafuerte@outlook.com",
+  address: "Jujuy, Argentina",
+  phone: "+54 388 5162847",
+  social: {
+    Linkedin: {
+      link: "https://www.linkedin.com/feed/",
+      icon: <LinkedInIcon />,
+      text: "hola",
+    },
+    Github: {
+      link: "https://github.com/nitovill",
+      icon: <GitHubIcon />,
+      text: "hola",
+    },
+  },
+};
