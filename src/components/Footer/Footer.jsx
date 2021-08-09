b1e593e2ca8935f6cfa221eafd5171878e4b4d10
nitovillafuerte <nitovillafuerte@outlook.com>
2021-08-08 23:00:33 -0300

    Portfolio footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Footer.css";
+import { Typography } from "@material-ui/core";
+import resumeData from "../../utils/resumeData";
 
 const Footer = () => {
-  return <div>Este es el componente Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name">{resumeData.name}</Typography>
+      </div>
+      <div className="fotter_right">
+        <Typography className="footer_coopyright">
+          Designed and Developed by{" "}
+          <a href="/" target="_blank">
+            Mariano Villafuerte
+          </a>
+          <br />
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
