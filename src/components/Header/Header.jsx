e1d217edaf23d4c041fb6da89f37fd218c2ebefb
nitovillafuerte <nitovillafuerte@outlook.com>
2021-08-11 18:22:34 -0300

    Portfolio contact me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,11 +12,58 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link, NavLink, withRouter } from "react-router-dom";
 import { HomeRounded, Telegram } from "@material-ui/icons";
 import resumeData from "../../utils/resumeData";
-import CustomBtn from "../Button/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Modal, TextField } from "@material-ui/core";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 import "./Header.css";
+import CustomBtn from "../Button/Button";
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Header = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
   const pathName = props?.location?.pathname;
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const Submit = () => {
+    console.log("hola");
+    const msg = {
+      to: "nitovillafuerte@outlook.com",
+      from: "nitovillafuerte@outlook.com", // aqui hay que poner el correo de la pag
+      subject: "Trekker Rent",
+      text: "Gracias por confiar en trekker!",
+      html: "<h2>Gracias por confiar en trekker!</h2>",
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(console.log(error));
+      });
+  };
   return (
     <Navbar expand="lg" sticky="top" className="header">
       <Nav.Link as={NavLink} className="header_navlink" to="/">
@@ -53,7 +100,40 @@ const Header = (props) => {
             </a>
           ))}
         </div>
-        <CustomBtn text="Hire me" icon={<Telegram />} />
+
+        <Button
+          className="custom_btn"
+          endIcon={<Telegram />}
+          onClick={handleOpen}
+        >
+          <span className="btn_text">Contact me</span>
+        </Button>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h6>Contact Form</h6>
+              <div style={{ paddingBottom: "25px" }}>
+                <TextField fullWidth name="name" label="Name" />
+                <TextField fullWidth name="email" label="E-mail" />
+                <TextField fullWidth name="message" label="Message" />
+              </div>
+
+              <CustomBtn text="Submit" onClick={Submit} />
+              <button onClick={Submit}></button>
+            </div>
+          </Fade>
+        </Modal>
       </Navbar.Collapse>
     </Navbar>
   );
